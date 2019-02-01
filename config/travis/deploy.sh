f82d5268d2c5c7ed27ba9fa158eabb879942eec9
Fabio Borriello <fborriello@expedia.com>
2019-02-01 15:42:25 +0100

    testing automatic release

--- config/travis/deploy.sh
+++ config/travis/deploy.sh
@@ -5,9 +5,9 @@ set -e
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   if [ ! -z "$TRAVIS_TAG" ]; then
     echo "Deploying release"
-    mvn clean deploy --settings config/travis/mvn-settings.xml -B -U -P sonatype-oss-release "$@" -DskipTests=true
+    mvn clean deploy --settings mvn-settings.xml -B -U -P sonatype-oss-release "$@" -DskipTests=true
   else
     echo "Deploying snapshot"
-    mvn clean deploy --settings config/travis/mvn-settings.xml -B -U -P -DskipTests=true
+    mvn clean deploy --settings mvn-settings.xml -B -U -P -DskipTests=true
   fi
 fi
