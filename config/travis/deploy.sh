88e38a37868c421d117dea0f2b927b46c038fe91
Fabio Borriello <fborriello@expedia.com>
2019-02-02 17:41:08 +0100

    deploying with script

--- config/travis/deploy.sh
+++ config/travis/deploy.sh
@@ -5,9 +5,13 @@ set -e
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   if [ ! -z "$TRAVIS_TAG" ]; then
     echo "Deploying release"
-    mvn clean deploy --settings mvn-settings.xml -B -U -P sonatype-oss-release "$@" -DskipTests=true
+    gpg --import config/travis/private-key.gpg
+#    mvn versions:set -DnewVersion=${TRAVIS_TAG}
+    mvn clean deploy --settings mvn-settings.xml -P release -DskipTests=true
+#    mvn clean deploy --settings mvn-settings.xml -B -U -P sonatype-oss-release "$@" -DskipTests=true
   else
     echo "Deploying snapshot"
-    mvn clean deploy --settings mvn-settings.xml -B -U -P -DskipTests=true
+    gpg --import config/travis/private-key.gpg
+    mvn clean deploy --settings mvn-settings.xml -P release -B -U -P -DskipTests=true
   fi
 fi
