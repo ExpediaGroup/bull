000da1761c2fb04f0d0ae6e7912afd0690f6c87a
Fabio Borriello <fborriello@expedia.com>
2019-02-02 18:03:59 +0100

    encoded private key

--- config/travis/deploy.sh
+++ config/travis/deploy.sh
@@ -6,12 +6,12 @@ if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   if [ ! -z "$TRAVIS_TAG" ]; then
     echo "Deploying release"
     gpg --import config/travis/private-key.gpg
-#    mvn versions:set -DnewVersion=${TRAVIS_TAG}
+    mvn versions:set -D newVersion=${TRAVIS_TAG}
     mvn clean deploy --settings config/travis/mvn-settings.xml -P release -DskipTests=true
-#    mvn clean deploy --settings mvn-settings.xml -B -U -P sonatype-oss-release "$@" -DskipTests=true
   else
     echo "Deploying snapshot"
+    echo ${TRAVIS_TAG}
     gpg --import config/travis/private-key.gpg
-    mvn clean deploy --settings config/travis/mvn-settings.xml -P release -B -U -P -DskipTests=true
+    mvn clean deploy --settings config/travis/mvn-settings.xml -B -U -P -DskipTests=true
   fi
 fi
