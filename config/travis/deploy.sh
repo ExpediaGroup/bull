bedd75c337e057a8c1e80732c194d42e865a3733
Fabio Borriello <borriello.fabio@gmail.com>
2019-02-03 06:56:24 +0100

    fixed travis config

--- config/travis/deploy.sh
+++ config/travis/deploy.sh
@@ -8,10 +8,10 @@ if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     gpg --import config/travis/private-key.gpg
     echo ${TRAVIS_TAG}
     mvn versions:set -D newVersion=${TRAVIS_TAG}
-    mvn clean deploy --settings config/travis/mvn-settings.xml -P release -DskipTests=true
+    mvn clean deploy --settings config/travis/mvn-settings.xml -Prelease -DskipTests=true
   else
     echo "Deploying snapshot"
     gpg --import config/travis/private-key.gpg
-    mvn clean deploy --settings config/travis/mvn-settings.xml -B -U -P -DskipTests=true
+    mvn clean deploy --settings config/travis/mvn-settings.xml -B -U -Prelease -DskipTests=true
   fi
 fi
