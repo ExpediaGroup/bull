2955b431f6f0687ce3966c8f13cdd6a10e532cea
Fabio Borriello <fborriello@expedia.com>
2019-02-04 10:39:30 +0100

    simplified release step

--- config/travis/deploy.sh
+++ config/travis/deploy.sh
@@ -3,15 +3,13 @@
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  if [ ! -z "$TRAVIS_TAG" ]; then
+  if [ "$TRAVIS_BRANCH" == 'master' ]; then
     echo "Deploying release"
     gpg --import config/travis/private-key.gpg
-    echo ${TRAVIS_TAG}
-    mvn versions:set -D newVersion=${TRAVIS_TAG}
     mvn deploy --settings config/travis/mvn-settings.xml -B -U -Prelease -DskipTests=true
-#  else
-#    echo "Deploying snapshot"
-#    gpg --import config/travis/private-key.gpg
-#    mvn deploy --settings config/travis/mvn-settings.xml -B -U -Prelease -DskipTests=true
-#  fi
+  else
+    echo "Deploying snapshot"
+    gpg --import config/travis/private-key.gpg
+    mvn deploy --settings config/travis/mvn-settings.xml -B -U -Prelease -DskipTests=true
+  fi
 fi
