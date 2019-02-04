dd8a59ad6336b2817c5300c6d6557f38e045e0be
Fabio Borriello <fborriello@expedia.com>
2019-02-04 06:31:28 +0100

    - Removed spring cache dependency as not needed - Added automatic snapshot release on tagged branches

--- config/travis/deploy.sh
+++ config/travis/deploy.sh
@@ -9,9 +9,9 @@ if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo ${TRAVIS_TAG}
     mvn versions:set -D newVersion=${TRAVIS_TAG}
     mvn deploy --settings config/travis/mvn-settings.xml -B -U -Prelease -DskipTests=true
-  else
-    echo "Deploying snapshot"
-    gpg --import config/travis/private-key.gpg
-    mvn deploy --settings config/travis/mvn-settings.xml -B -U -Prelease -DskipTests=true
-  fi
+#  else
+#    echo "Deploying snapshot"
+#    gpg --import config/travis/private-key.gpg
+#    mvn deploy --settings config/travis/mvn-settings.xml -B -U -Prelease -DskipTests=true
+#  fi
 fi
