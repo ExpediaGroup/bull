4b0a2479dfd2dab68461f8ed9b7fa5c9669498e4
Fabio Borriello <borriello.fabio@gmail.com>
2019-05-24 08:07:47 +0200

    fixed minor in javadoc

--- bull-common/src/main/java/com/hotels/beans/validator/Validator.java
+++ bull-common/src/main/java/com/hotels/beans/validator/Validator.java
@@ -49,7 +49,6 @@ public interface Validator {
      * @param <K> the object class
      * @param k the object to check
      * @throws com.hotels.beans.error.InvalidBeanException {@link com.hotels.beans.error.InvalidBeanException} if the validation fails
-     * @return
      */
     <K> void validate(K k);
 
