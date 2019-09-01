2d3420b1fd09794e8e1af867c6ab287cbf390af9
Fabio Borriello <borriello.fabio@gmail.com>
2019-09-01 15:48:19 +0200

    Started bean-utils-library module relocation

---
+++ bull-bean-transformer/src/main/java/com/hotels/beans/transformer/Transformer.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2019 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hotels.beans.transformer;
+
+/**
+ * Utility methods for all objects transformation.
+ * @deprecated This class will be removed since version 1.6.0 please use {@link com.hotels.transformer.Transformer} instead.
+ */
+@Deprecated(since = "1.6.0", forRemoval = true)
+public interface Transformer extends com.hotels.transformer.Transformer {
+}
