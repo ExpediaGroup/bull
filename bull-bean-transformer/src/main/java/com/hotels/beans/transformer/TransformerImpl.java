2ca12c3657d6727848cd53a88e32ff0153b3229a
Fabio Borriello <borriello.fabio@gmail.com>
2019-11-18 15:02:40 +0100

    Modified exception message

--- bull-bean-transformer/src/main/java/com/hotels/beans/transformer/TransformerImpl.java
+++ bull-bean-transformer/src/main/java/com/hotels/beans/transformer/TransformerImpl.java
@@ -114,8 +114,9 @@ public class TransformerImpl extends AbstractBeanTransformer {
             String errorMsg;
             if (!classUtils.areParameterNamesAvailable(constructor)) {
                 errorMsg = "Constructor's parameters name have been removed from the compiled code. "
-                + "This caused a problems during the: " + targetClass.getSimpleName() + " injection, "
-                + "consider to add the property: <parameters>true</parameters> to your maven-compiler configuration";
+                + "This caused a problems during the: " + targetClass.getSimpleName() + " injection. "
+                + "Consider to use: @ConstructorArg annotation: https://github.com/HotelsDotCom/bull#different-field-names-defining-constructor-args "
+                + "or add the property: <parameters>true</parameters> to your maven-compiler configuration";
             } else {
                 errorMsg = "Constructor invoked with wrong arguments. Expected: " + constructor + "; Found: "
                         + getFormattedConstructorArgs(targetClass, constructorArgs)
