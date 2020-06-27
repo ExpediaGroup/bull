29dd880363c12b8243e0339f9e622f5b3962ca4e
Fabio Borriello <borriello.fabio@gmail.com>
2020-06-27 15:44:31 +0200

    Update bull-converter/src/test/java/com/hotels/beans/conversion/processor/ConversionProcessorFactoryTest.java
    Co-authored-by: Matteo Mirk <polaretto@gmail.com>

--- bull-converter/src/test/java/com/hotels/beans/conversion/processor/ConversionProcessorFactoryTest.java
+++ bull-converter/src/test/java/com/hotels/beans/conversion/processor/ConversionProcessorFactoryTest.java
@@ -71,7 +71,7 @@ public class ConversionProcessorFactoryTest {
         Optional<ConversionProcessor> actual = underTest.getConversionProcessor(Pair.class);
 
         // THEN
-        assertThat(actual.isEmpty()).isTrue();
+        assertThat(actual).isEmpty();
     }
 
     /**
